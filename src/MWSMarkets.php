babe67a37cb828e34aac5b2ecfdd105ad8b0091a
gavin <gavin.jiang@astroai.com.cn>
2021-05-20 11:41:34 +0800

    x

---
+++ src/MWSMarkets.php
@@ -0,0 +1,32 @@
+<?php
+namespace MCS;
+
+class System {
+    /**
+     * Mws API市场配置
+     */
+    public static $MwsMarkets = [
+        'A2EUQ1WTGCTBG2' => 'mws.amazonservices.ca',
+        'A1MQXOICRS2Z7M' => 'mws.amazonservices.ca',
+        'ATVPDKIKX0DER' => 'mws.amazonservices.com',
+        'A2ZV50J4W1RKNI' => 'mws.amazonservices.com',
+        'A1AM78C64UM0Y8' => 'mws.amazonservices.com.mx',
+        'A1PA6795UKMFR9' => 'mws-eu.amazonservices.com',
+        'A38D8NSA03LJTC' => 'mws-eu.amazonservices.com',
+        'A1RKKUPIHCS9HS' => 'mws-eu.amazonservices.com',
+        'A13V1IB3VIYZZH' => 'mws-eu.amazonservices.com',
+        'A1ZFFQZ3HTUKT9' => 'mws-eu.amazonservices.com',
+        'A21TJRUUN4KGV' => 'mws.amazonservices.in',
+        'APJ6JRA9NG5V4' => 'mws-eu.amazonservices.com',
+        'A1F83G8C2ARO7P' => 'mws-eu.amazonservices.com', // UK
+        'AZMDEXL2RVFNN' => 'mws-eu.amazonservices.com',
+        'A1VC38T7YXB528' => 'mws.amazonservices.jp', // JP
+        'A1VN0HAN483KP2' => 'mws.amazonservices.jp', // JP
+        'AAHKV2X7AFYLW' => 'mws.amazonservices.com.cn', //CN
+        'A39IBJ37TRP1C6' => 'mws.amazonservices.com.au', // AU
+        'A2Q3Y263D00KWC' => 'mws.amazonservices.com', // BR
+        'A1805IZSGTT6HS' => 'mws-eu.amazonservices.com', // NL
+        'A2NODRKZP88ZB9' => 'mws-eu.amazonservices.com', // Se
+        'A1C3SOZRARQ6R3' => 'mws-eu.amazonservices.com', // Pl
+    ];
+}
